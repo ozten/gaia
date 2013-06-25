aa7d535dc6d16aacbe410660c98befb50e81eeb8
MrDHat <ktyaks@gmail.com>
2013-06-26 02:13:07 +0530

    Added user GET and POST methods on /user for key-server [Needs Work]

--- apps/settings/js/PiCl/picl.js
+++ apps/settings/js/PiCl/picl.js
@@ -13,7 +13,7 @@
 var PiCl = {
 
   _serverHost: null,
-  _keyServerHost: null,
+  _keyServerHost: 'http://127.0.0.1:8090',
   _syncFlag: null, // if sync_flag is set then sync service is active
 
   init: function picl_init(options) {
@@ -45,7 +45,23 @@ var PiCl = {
   createUser: function picl_createUser(args) {
     args = args || {};
     if (!args.email) {
-      handleError('No email in argument to createUser' + args);
+      handleError('No email in argument to createUser ' + args);
+    }
+
+    else {
+      var xhr = new XMLHttpRequest({
+        mozSystem: true
+      });
+      xhr.onreadystatechange = function() {
+        if (xhr.status === 200 && xhr.readystatechange === 4) {
+          console.log(xhr.response);
+        }
+      };
+      xhr.onerror = function() {
+        handleError(xhr.response);
+      };
+      xhr.open('POST', _keyServerHost + '/user', true);
+      xhr.send(args);
     }
   },
 
@@ -55,5 +71,44 @@ var PiCl = {
   */
   getUser: function picl_getUser(args) {
     args = args || {};
+    if (!args.email) {
+      handleError('No email in argument to getUser ' + args);
+    }
+
+    else {
+      var xhr = new XMLHttpRequest({
+        mozSystem: true
+      });
+      xhr.onreadystatechange = function() {
+        if (xhr.status === 200 && xhr.readystatechange === 4) {
+          console.log(xhr.response);
+        }
+      };
+      xhr.onerror = function() {
+        alert(xhr.response);
+        //handleError(xhr.response);
+      };
+      xhr.open('GET', _keyServerHost + '/user/?email=' + args.email, true);
+      xhr.send();
+    }
   }
 };
+
+
+//XXX Remove these buttons when done testing.
+var getUser_button = document.getElementById('mock-picl-getUser');
+getUser_button.addEventListener('click', function() {
+
+  PiCl.getUser({'email': 'ktyaks@gmail.com'});
+  //alert('mock-picl-getUser');
+
+});
+
+var createUser_button = document.getElementById('mock-picl-createUser');
+createUser_button.addEventListener('click', function() {
+
+  //PiCl.createUser({'email':'ktyaks@gmail.com'})
+  alert('mock-picl-createUser');
+
+});
+///
