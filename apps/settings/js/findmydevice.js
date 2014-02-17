1aaa6e66e90384aac680412803e1e7083e7b8a91
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2014-02-17 10:12:57 -0300

    (review comment) Turn some callbacks into methods in the Settings app.

--- apps/settings/js/findmydevice.js
+++ apps/settings/js/findmydevice.js
@@ -3,78 +3,88 @@
 'use strict';
 
 var FindMyDevice = {
+  _api: null,
   _personaIframe: null,
 
   init: function fmd_init() {
     var self = this;
-    var _ = navigator.mozL10n.get;
-    console.log('initializing findmydevice');
 
-    SettingsListener.observe('findmydevice.enabled', false, function(value) {
-      console.log('findmydevice.enabled = ' + value);
-
-      var desc = document.getElementById('findmydevice-desc');
-      desc.textContent = value ? _('enabled') : _('disabled');
-
-      var chbox = document.querySelector('#findmydevice-enabled input');
-      chbox.checked = value;
-      chbox.disabled = false;
-    });
-
-    SettingsListener.observe('findmydevice.registered', false, function(value) {
-      var signin = document.getElementById('findmydevice-signin');
-      signin.hidden = value;
-
-      var settings = document.getElementById('findmydevice-settings');
-      settings.hidden = !value;
-    });
-
-    var api = null;
     var lock = SettingsListener.getSettingsLock();
     lock.get('findmydevice.api_url').onsuccess = function() {
-      api = this.result['findmydevice.api_url'];
+      self._api = this.result['findmydevice.api_url'];
     };
 
-    window.addEventListener('message', function(event) {
-      document.body.removeChild(self._personaIframe);
-      self._personaIframe = null;
-
-      if (!event.data) {
-        return;
-      }
-
-      SettingsListener.getSettingsLock().set({
-        'findmydevice.assertion': event.data
-      });
-
-      SettingsListener.getSettingsLock().set({
-        'findmydevice.enabled': true
-      });
-    }, false);
+    SettingsListener.observe('findmydevice.enabled', false,
+      this._setEnabled.bind(this));
+    SettingsListener.observe('findmydevice.registered', false,
+      this._setRegistered.bind(this));
 
     var loginButton = document.getElementById('findmydevice-login');
-    loginButton.addEventListener('click', function() {
-      if (self._personaIframe !== null) {
-        return;
-      }
+    loginButton.addEventListener('click', this._requestLogin.bind(this));
 
-      self._personaIframe = document.createElement('iframe');
-      self._personaIframe.src = api + '/static/persona_iframe.html';
-      console.log(self._personaIframe.src);
-      document.body.appendChild(self._personaIframe);
-    });
+    var checkbox = document.querySelector('#findmydevice-enabled input');
+    checkbox.addEventListener('change', this._onCheckboxChanged.bind(this));
 
-    var chbox = document.querySelector('#findmydevice-enabled input');
-    chbox.onchange = function fmd_toggle() {
+    window.addEventListener('message', this._onAssertionObtained.bind(this));
+  },
+
+  _setEnabled: function fmd_set_enabled(value) {
+    var _ = navigator.mozL10n.get;
+
+    var desc = document.getElementById('findmydevice-desc');
+    desc.textContent = value ? _('enabled') : _('disabled');
+
+    var checkbox = document.querySelector('#findmydevice-enabled input');
+    checkbox.checked = value;
+    checkbox.disabled = false;
+  },
+
+  _setRegistered: function fmd_set_registered(value) {
+    var signin = document.getElementById('findmydevice-signin');
+    signin.hidden = value;
+
+    var settings = document.getElementById('findmydevice-settings');
+    settings.hidden = !value;
+  },
+
+  _requestLogin: function fmd_requestLogin() {
+    if (this._personaIframe !== null) {
+      return;
+    }
+
+    this._personaIframe = document.createElement('iframe');
+    this._personaIframe.src = this._api + '/static/persona_iframe.html';
+    document.body.appendChild(this._personaIframe);
+  },
+
+  _onCheckboxChanged: function fmd_on_checkbox_changed() {
+      var checkbox = document.querySelector('#findmydevice-enabled input');
+
       SettingsListener.getSettingsLock().set({
-        'findmydevice.enabled': this.checked
+        'findmydevice.enabled': checkbox.checked
       }).onerror = function() {
-        chbox.disabled = false;
+        checkbox.disabled = false;
       };
 
-      this.disabled = true;
-    };
-  }
+      checkbox.disabled = true;
+  },
+
+  _onAssertionObtained: function fmd_on_assertion_obtained(event) {
+    document.body.removeChild(this._personaIframe);
+    this._personaIframe = null;
+
+    if (!event.data) {
+      return;
+    }
+
+    SettingsListener.getSettingsLock().set({
+      'findmydevice.assertion': event.data
+    });
+
+    SettingsListener.getSettingsLock().set({
+      'findmydevice.enabled': true
+    });
+  },
 };
 
 navigator.mozL10n.ready(FindMyDevice.init.bind(FindMyDevice));
