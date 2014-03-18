2af2f31ff0b6c35a9d93bb84b8f21d5d98059ecb
Jed Parsons <jedp@me.com>
2014-03-18 16:30:33 -0700

    added contact change listener

--- apps/system/services/contacts/js/backup.js
+++ apps/system/services/contacts/js/backup.js
@@ -4,16 +4,38 @@
 
 'use strict';
 
+function findContactById(contactID) {
+  var options = {
+    filterBy: ['id'],
+    filterOp: 'equals',
+    filterValue: contactID
+  };
+  var promise = new Promise(function done(resolve, reject) {
+    var request = navigator.mozContacts.find(options);
+
+    request.onsuccess = function(event) {
+      var result = event.target.result[0];
+      console.log('** found: ' + JSON.stringify(result));
+      resolve(result);
+    };
+
+    request.onerror = function(error) {
+      console.error('** got error: ' + error);
+      reject(error);
+    };
+  });
+
+  return promise;
+}
+
 var BackupService = {
-  enabled: false,
+  enabled: true,    // we'll set this with a pref later
   queue: [],
 
   init: function() {
-    console.log('** backup service init');
-    console.log(' mozContacts is ' + typeof mozContacts);
+    var self = this;
 
-    mozContacts.oncontactchange = function(event) {
-      console.log('contact change');
+    navigator.mozContacts.oncontactchange = function(event) {
       if (this.enabled) {
         this.enqueue(event.contactID);
       }
@@ -22,6 +44,14 @@ var BackupService = {
 
   enqueue: function(contactID) {
     console.log('** here we would enqueue ' + contactID);
+    findContactById(contactID).then(
+      function resolve(result) {
+        console.log('** yay! ' + JSON.stringify(result));
+      },
+      function reject(error) {
+        console.error(error);
+      }
+    );
   },
 
 };
